4363182e5df5a41cd01d9055a607476467712dc8
FilipeRPatricio <202300133@estudantes.ips.pt>
2026-05-13 19:29:56 +0100

    commit inicial

---
+++ PrimeFormula.py
@@ -0,0 +1,14 @@
+﻿def is_prime(n: int) -> bool:
+ if n < 2:
+    return False
+ if n in (2, 3):
+  return True
+ if n % 2 == 0 or n % 3 == 0:
+  return False
+
+ divisor = 5
+ while divisor * divisor <= n:
+  if n % divisor == 0 or n % (divisor + 2) == 0:
+   return False
+  divisor += 6
+ return True
--- TestFile.py
+++
@@ -1,4 +0,0 @@
-﻿#this is a test file
-from contextlib import nullcontext
-
-if __name__ == '__main__': nullcontext
